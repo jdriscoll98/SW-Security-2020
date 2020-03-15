8d2fd67a49c99fd31a2275a0418a5f5b2fc1ab90
Jack Driscoll <jdriscoll98@ufl.edu>
2020-03-15 16:02:50 -0400

    Made landing and create account pages

--- web/app.py
+++ web/app.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template
  
 app = Flask(__name__)
  
- 
+
 @app.route('/')
-def hello():
+def landing_page():
     return render_template('landing_page.html')
+
+@app.route('/create-account')
+def create_account():
+    return render_template('create_account.html')
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
